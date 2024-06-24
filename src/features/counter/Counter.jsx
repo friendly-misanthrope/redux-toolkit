21cef104d2674d49d524a98c0c0be13575a5f70d
friendly-misanthrope <nickthedevguy@gmail.com>
2024-06-24 13:08:46 -0700

    created input for incrementAmount and buttons with click handlers for resetting the counter and incrementByAmount

--- src/features/counter/Counter.jsx
+++ src/features/counter/Counter.jsx
@@ -1,9 +1,24 @@
 import { useSelector, useDispatch } from 'react-redux';
-import { increment, decrement } from './counterSlice';
+import { useState } from 'react';
+import {
+  increment,
+  decrement,
+  reset,
+  incrementByAmount
+} from './counterSlice';
 
 const Counter = () => {
   const count = useSelector((state) => state.counter.count);
   const dispatch = useDispatch();
+
+  const [incrementAmount, setIncrementAmount] = useState(0);
+
+  const addValue = Number(incrementAmount || 0);
+
+  const resetAll = () => {
+    setIncrementAmount(0);
+    dispatch(reset());
+  }
 
   return (
     <section>
@@ -11,6 +26,11 @@ const Counter = () => {
       <div>
         <button onClick={() => dispatch(increment())}>+</button>
         <button onClick={() => dispatch(decrement())}>-</button>
+      </div>
+      <input type="number" value={incrementAmount} onChange={(e) => setIncrementAmount(e.target.value)} />
+      <div>
+        <button onClick={() => dispatch(incrementByAmount(addValue))}>Add Amount</button>
+        <button onClick={resetAll}>Reset Counter</button>
       </div>
     </section>
   );
